0ce2766b5f6186d8c56f45585476f36846a37b77
iimmuunnee <glgmsl@naver.com>
2023-09-27 12:45:22 +0900

    genre

--- netflix-app/src/components/MovieCard.jsx
+++ netflix-app/src/components/MovieCard.jsx
@@ -7,20 +7,21 @@ import { useSelector } from 'react-redux';
 const MovieCard = ({ info }) => {
 
   const {genreList} = useSelector((state) => state.movie)
+  // 장르 번호별 리스트 
   console.log(genreList);
 
   const backgroundImg = info.poster_path
   const movieTitle = info.title
-  const movieGenre = info.genre_ids
+  const movieGenre = info.genre_ids // 영화 별 장르 리스트
   const movieVote_average = info.vote_average
   const movieAdult = info.adult
 
 
   const div_Style = {
-    backgroundImage: `url(https://www.themoviedb.org/t/p/w355_and_h200_multi_faces${backgroundImg})`,
+    backgroundImage: `url(https://www.themoviedb.org/t/p/w220_and_h330_face${backgroundImg})`,
     margin: "10px",
-    height: "200px",
-    width: "350px",
+    height: "330px",
+    width: "220px",
   }
 
   return (
@@ -29,11 +30,13 @@ const MovieCard = ({ info }) => {
         <div className='movieInfo'>
           <h2>{movieTitle}</h2>
           <Stack direction="horizontal" gap={2}>
-            {movieGenre.map((item) => {
+            {movieGenre.map((item) => (
               <Badge bg="danger" key={item}>
-                
+                {genreList.find((genre) => (
+                  genre.id === item
+                )).name}
               </Badge>
-            })}
+            ))}
           </Stack>
           <p style={{fontWeight : "bold"}}>평점 : {movieVote_average} | {movieAdult?"성인":"청소년"}</p>
         </div>
